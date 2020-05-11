0b8608fa5f742a7a3a7e6244937b9a5bb7f4bc32
BugenZhao <i@bugenzhao.com>
2020-05-11 20:55:45 +0800

    Updated: mod machine

--- Sources/Turing/Mod.swift
+++ Sources/Turing/Mod.swift
@@ -8,8 +8,10 @@
 import Foundation
 import TuringLib
 
+infix operator ≡: MultiplicationPrecedence
+
 @discardableResult
-func mod(x: Int, y: Int) -> Int {
+func ≡(x: Int, y: Int) -> Int {
     guard x > y && y >= 1 else { fatalError() }
 
     enum Symbol: String, CustomStringConvertible, CustomLaTeXStringConvertible {
@@ -40,29 +42,28 @@ func mod(x: Int, y: Int) -> Int {
     typealias Inst = Instruction<State, Symbol>
 
     let instructions: [Inst] = [
-        Inst(.start, [.👉], .readX, [.👉], [.R]),
-        Inst(.readX, [.ichi], .toY, [.👉], [.R]),
-        Inst(.readX, [.blank], .revY, [.👉], [.R]),
-        Inst(.toY, [.ichi], .toY, [.ichi], [.R]),
-        Inst(.toY, [.blank], .decY, [.blank], [.R]),
-        Inst(.decY, [.rei], .decY, [.rei], [.R]),
-        Inst(.decY, [.ichi], .testY, [.rei], [.R]),
-        Inst(.testY, [.ichi], .toX, [.ichi], [.L]),
-        Inst(.testY, [.👈], .resY, [.👈], [.L]),
-        Inst(.resY, [.rei], .resY, [.ichi], [.L]),
-        Inst(.resY, [.blank], .toX, [.blank], [.L]),
-        Inst(.toX, [.rei], .toX, [.rei], [.L]),
-        Inst(.toX, [.ichi], .toX, [.ichi], [.L]),
-        Inst(.toX, [.blank], .toX, [.blank], [.L]),
-        Inst(.toX, [.👉], .readX, [.👉], [.R]),
-
-        Inst(.revY, [.rei], .revY, [.ichi], [.R]),
-        Inst(.revY, [.ichi], .revY, [.👈], [.R]),
-        Inst(.revY, [.👈], .halt, [.👈], [.S]),
+        (.start, [.👉]) ~~> (.readX, [.👉], [.R]),
+        (.readX, [.ichi]) ~~> (.toY, [.👉], [.R]),
+        (.readX, [.blank]) ~~> (.revY, [.👉], [.R]),
+        (.toY, [.ichi]) ~~> (.toY, [.ichi], [.R]),
+        (.toY, [.blank]) ~~> (.decY, [.blank], [.R]),
+        (.decY, [.rei]) ~~> (.decY, [.rei], [.R]),
+        (.decY, [.ichi]) ~~> (.testY, [.rei], [.R]),
+        (.testY, [.ichi]) ~~> (.toX, [.ichi], [.L]),
+        (.testY, [.👈]) ~~> (.resY, [.👈], [.L]),
+        (.resY, [.rei]) ~~> (.resY, [.ichi], [.L]),
+        (.resY, [.blank]) ~~> (.toX, [.blank], [.L]),
+        (.toX, [.rei]) ~~> (.toX, [.rei], [.L]),
+        (.toX, [.ichi]) ~~> (.toX, [.ichi], [.L]),
+        (.toX, [.blank]) ~~> (.toX, [.blank], [.L]),
+        (.toX, [.👉]) ~~> (.readX, [.👉], [.R]),
+        (.revY, [.rei]) ~~> (.revY, [.ichi], [.R]),
+        (.revY, [.ichi]) ~~> (.revY, [.👈], [.R]),
+        (.revY, [.👈]) ~~> (.halt, [.👈], [.S]),
     ]
 
-    instructions.forEach { print($0.latexDescription + " &{}\\cdots{}&\n\\text{Description}\\\\") }
-    print()
+    // instructions.forEach { print($0.latexDescription + " &{}\\cdots{}&\n\\text{Description}\\\\") }
+    // print()
 
     var tape = [Symbol?]()
     tape.append(.👉)
@@ -74,7 +75,7 @@ func mod(x: Int, y: Int) -> Int {
     try! machine.addInstruction(from: instructions)
     machine.tapes[0].tape = tape
 
-    machine.run(verbose: .latex)
+    machine.run(verbose: .half)
 
     let expected = x % y
     let tm = machine.tapes[0].count(.ichi)
